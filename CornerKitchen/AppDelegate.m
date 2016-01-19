bd05cad7fee0fc640122db14dd3ee537c539d239
LOCKED <danrudolf@gmail.com>
2016-01-19 18:05:44 -0500

    Update AppDelegate.m

--- CornerKitchen/AppDelegate.m
+++ CornerKitchen/AppDelegate.m
@@ -17,8 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    [Parse setApplicationId:@"HkRTw4cVCl4rX8aXpzH2njpM9Oulptji90VGr7fk"
-                  clientKey:@"j1pI1viI2dY53O2MfOemWLbtYz9v2s7nwlF3FoJk"];
+    [Parse setApplicationId:@"********************"
+                  clientKey:@"********************"];
 
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
 
